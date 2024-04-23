d1ba3071fed0e9789c1f8e73aab8f5db4255ca82
ronwilljr <10ronaldwilliams10@gmail.com>
2024-04-23 11:48:13 -0500

    mod 9 sol

---
+++ module9-solution/categories.controller.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+    angular.module('MenuApp').controller('categoriesController', categoriesController);
+    categoriesController.$inject = ['MenuDataService', 'categories'];
+    function categoriesController(MenuDataService, categories) {
+        var categoriesCtrl = this;
+        categoriesCtrl.categories = categories;
+    }
+})();
